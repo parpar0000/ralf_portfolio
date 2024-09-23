e9c9784db8ded851c6901aa3cec6b316cfe61d5f
Ralf <ralfdiez23@gmail.com>
2024-09-23 22:07:35 +0400

    addning codes from HP

---
+++ script/script.js
@@ -0,0 +1,19 @@
+var menuItems = document.getElementById("menuItems");
+var hamburgerImg = document.querySelector(".hamburger-icon");
+var closeImg = document.querySelector(".close-icon");
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle() {
+  if(menuItems.style.maxHeight == "0px") {
+    menuItems.style.maxHeight = "200px";
+    hamburgerImg.style.display = "none";
+    closeImg.style.display = "inline-block"
+  } else {
+    menuItems.style.maxHeight = "0px";
+    hamburgerImg.style.display = "inline-block";
+    closeImg.style.display = "none"
+
+  }
+  
+}
